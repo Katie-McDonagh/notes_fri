d8830fc9f35d504049e6bf3606ca1c60a48f21b8
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-06-25 14:44:32 +0100

    change to test responses

--- note-controller.js
+++ note-controller.js
@@ -11,30 +11,13 @@
   exports.NoteController = NoteController;
 })(this);
     
-    
+document.addEventListener('DOMContentLoaded', function (event) {
+  var list = new NoteList
+  list.addNote("im a note")
+  var controller = new NoteController(list)
+  controller.renderHtml();
+  });   
 
 
-
-// (function (exports) {
-//   function NoteController(NoteList) {
-//     this.notelist = NoteList;
-//     this.notelist.addNote('Beer');
-//     this.view = new NoteListView(this.notelist);
-//   }
-
-//   NoteController.prototype.renderHtml = function () {
-//     var test = document.getElementById('app');
-//     test.innerHTML = this.view.htmlWrap();
-//   };
-
-//   exports.NoteController = NoteController;
-
-// })(this);
-
-// document.addEventListener('DOMContentLoaded', function (event) {
-//   var list = new NoteList
-//   var controller = new NoteController(list)
-//   controller.renderHtml();
-//   });
 
 
